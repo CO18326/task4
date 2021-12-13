15efdfb106fb9eca13520d5c993a508647cf7a7f
Ishan Gambhir <ishugambhir2001@gmail.com>
2021-12-13 10:20:12 +0530

    form validations

--- src/app/cart-view/cart-view.component.ts
+++ src/app/cart-view/cart-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-import { FormBuilder } from '@angular/forms';
+import { FormGroup,FormBuilder,Validators , ValidatorFn,ValidationErrors,AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-cart-view',
@@ -12,18 +12,32 @@ export class CartViewComponent implements OnInit {
   
   items=this.cartService.getItems();
   checkoutForm = this.formBuilder.group({
-    name: '',
+    name:  ['',this.Validator(/^[a-zA-Z]+\s{0,1}[a-zA-Z]+$/) ],
+    email: ['',this.Validator(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)],
     address: ''
   });
-  constructor(private cartService:CartService,private formBuilder:FormBuilder) { }
+  constructor(private cartService:CartService,private formBuilder:FormBuilder) {
+   }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
     this.items = this.cartService.clearCart();
+    if(this.checkoutForm.controls['name'].errors?.['forbiddenName'] || this.checkoutForm.controls['email'].errors?.['forbiddenName'] ){
+      alert("there is error in form")
+    }
+    else{
     alert("form submitted");
     this.checkoutForm.reset();
+    }
+  }
+
+  Validator(regex:RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = !(regex.test(control.value));
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
   }
 
 }
